08b43d83f2fcf910ce70eef2061818d2e53e7535
chompy23 <sdefederico1@gmail.com>
2024-06-12 16:44:11 -0300

    Commit 12/6

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoReporitory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoReporitory medicoReporitory;
+
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
         System.out.println("El request llego correctamente");
-        System.out.println(datosRegistroMedico);
+        medicoReporitory.save(new Medico(datosRegistroMedico));
      }
 }
--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -12,9 +12,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Direccion {
     private String calle;
-    private Integer numero;
-    private String ccomplemeto;
+    private String numero;
+    private String complemento;
     private String distrito;
     private String ciudad;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.complemento = direccion.comlemento();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+
+    }
+
 
 }
