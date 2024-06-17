bd1422f1c4330e4456ce72bdd0f8c74967f097ad
chompy23 <sdefederico1@gmail.com>
2024-06-17 11:27:17 -0300

    Commit 17/6

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedicos;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoReporitory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +24,9 @@ public class MedicoController {
     public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
         System.out.println("El request llego correctamente");
         medicoReporitory.save(new Medico(datosRegistroMedico));
+     }
+     @GetMapping
+     public Page<DatosListadoMedicos> listadoMedicos(@PageableDefault(size = 2)  Pageable paginacion){
+        return medicoReporitory.findAll(paginacion).map(DatosListadoMedicos::new);
      }
 }
