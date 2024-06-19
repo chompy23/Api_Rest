21a96077f96b6be53c571003463f13ee79d1c444
chompy23 <sdefederico1@gmail.com>
2024-06-19 15:40:27 -0300

    Commit 19/6 B

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -2,9 +2,10 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-
+@Component
 public class ApiApplication {
 
 	public static void main(String[] args) {
